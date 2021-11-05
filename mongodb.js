b746dc5f8fd10422c97d17f83ba489e6e88c2ae6
JeranDev <jeranjb@gmail.com>
2021-11-04 18:07:43 -0600

    add update function

--- mongodb.js
+++ mongodb.js
@@ -15,34 +15,22 @@ MongoClient.connect(
 
     const db = client.db(databaseName)
 
-    // db.collection('users').findOne(
-    //   { _id: new ObjectId('61808ec133ef644aa80b05b4') },
-    //   (error, user) => {
-    //     if (error) {
-    //       return console.log('Unable to fetch user.')
-    //     }
-
-    //     console.log(user)
-    //   }
-    // )
-
-    // db.collection('users')
-    //   .find({})
-    //   .count((error, count) => {
-    //     console.log(count)
-    //   })
-
-    db.collection('tasks').findOne(
-      { _id: new ObjectId('618090bff1b953ded8254601') },
-      (error, task) => {
-        console.log(task)
-      }
-    )
-
-    db.collection('tasks')
-      .find({ completed: false })
-      .toArray((error, tasks) => {
-        console.log(tasks)
+    db.collection('users')
+      .updateOne(
+        {
+          _id: new ObjectId('61808a54fa017170a1ade26e'),
+        },
+        {
+          $set: {
+            name: 'Mike',
+          },
+        }
+      )
+      .then(result => {
+        console.log(result)
+      })
+      .catch(error => {
+        console.log(error)
       })
   }
 )
